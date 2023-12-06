fa5e544d088b36c21c30a368b22515b6659d23ba
cgoodw2847 <149614676+cgoodw2847@users.noreply.github.com>
2023-12-06 00:23:00 -0500

    Update Updated Wind Speed Forecast w Plotting.py

--- Updated Wind Speed Forecast w Plotting.py
+++ Updated Wind Speed Forecast w Plotting.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
-from scipy.stats import weibull_min, kstest
 from sklearn.metrics import mean_squared_error
 #import os
 
@@ -23,11 +22,11 @@ dataset["Day of Week"] = df["Date and hour"].dt.day_name()
 dataset = df.set_index("Date and hour")
 dataset.index = pd.to_datetime(dataset.index)
 
-dates= [1415,	2158,
-10175,	10918,
-18959,	19702,
-27719,	28462,
-36479,	37222]
+dates= [6671,	7031,
+15431,	15791,
+24215,	24575,
+32975,	33335,
+41831,	41855]
 
 y2 = df.iloc[dates[8]:dates[9], 1]
 
@@ -159,58 +158,35 @@ mse = mean_squared_error(Rec_Test, predicted_speed)
 print(f"Mean Squared Error: {mse}")
 
 # Comparison Plot
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(9, 6))
 plt.plot(df.loc[test_data, "Date and hour"], Rec_Test.values, color="green", label="Recorded Data")
 plt.plot(df.loc[test_data, "Date and hour"], predicted_speed, color="red", label="Predicted Data")
 plt.xlabel('Date and Hour')
 plt.ylabel("Speed")
-plt.title(f'May 1st - Comparison of Recorded and Predicted Wind Speed (MSE={mse:.4f})')
+plt.title(f'October 10th - Comparison of Recorded and Predicted Wind Speed (MSE={mse:.4f})')
 plt.legend()
 plt.show()
 
 #%% Predicted Data Distribution Plot
 
-# Predicted Data Distribution Plot
-params = weibull_min.fit(predicted_speed.flatten(), floc=0)
-x = np.linspace(0, np.max(predicted_speed), 100)
-pdf = weibull_min.pdf(x, *params)
-
 # Create histogram
-plt.figure(figsize=(12, 6))
-sns.histplot(predicted_speed[:,0], stat="density", bins=50, kde=False, label="Predicted Data")
+plt.figure(figsize=(9, 6))
+sns.histplot(predicted_speed, bins=range(0,18), kde=False, label="Predicted Data")
 
-# Overlay the fitted distribution
-plt.plot(x, pdf, 'k-', label='Weibull Fit')
-
-# Calculate the expected value
-expected_value = np.mean(predicted_speed)
-
-# Add vertical red dashed line at the expected value
-plt.axvline(x=expected_value, color='red', linestyle='--', label=f'Expected Value: {expected_value:.2f}')
-
-# KS test
-_, p_value_ks = kstest(predicted_speed.flatten(), 'weibull_min', args=params)
-
-plt.title(f'Distribution of Predicted Wind Speed (KS p-value={p_value_ks:.4f})')
+plt.title('October 10th - Distribution of Predicted Wind Speed')
 plt.xlabel('Wind Speed')
-plt.ylabel('Density')
+plt.ylabel('Hours')
 plt.legend()
 plt.show()
 
 # Forming a Density Matrix
-hist_data, bin_edges = np.histogram(predicted_speed.flatten(), bins=50, density=True)
+hist_data, bin_edges = np.histogram(predicted_speed, bins=range(0,11))
 bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
 
 density_matrix = pd.DataFrame({
     "Wind Speed": bin_centers, "Density": hist_data})
 
 #%% Daily Power Output Calculation
-
-# Scale to 24hr Matrix
-matrix = np.array([np.ones(50),(dates[9]-dates[8]+1)*np.ones(50)])
-daily_scale = matrix.T
-
-daily_density = density_matrix*daily_scale
 
 # Power Curve for Wind Turbine
 wind_speed = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
@@ -225,15 +201,16 @@ power_fit = poly_function(wind_speed)
 #plt.scatter(wind_speed, power_kWe, label='Original Data')
 #plt.plot(wind_speed, power_fit, label=f'Polynomial Fit (Degree {degree})', color='red')
 #plt.xlabel('Wind Speed')
-#plt.ylabel('Power (kWe)')
+#plt.ylabel('Power (kWh)')
 #plt.legend()
 #plt.show()
 
 # Power Calculation
-bin_wind_speeds = daily_density.iloc[:,0]
+bin_wind_speeds = density_matrix.iloc[:,0]
 power_generated = poly_function(bin_wind_speeds)
+power_generated[power_generated < 0] = 0
 
-bin_density = daily_density.iloc[:,1]
+bin_density = density_matrix.iloc[:,1]
 
-daily_generation = np.dot(bin_wind_speeds,bin_density)
-print(f'Daily Power Generated: {daily_generation:.2f} kWe')
+daily_generation = np.dot(power_generated,bin_density)
+print(f'Daily Power Generated: {daily_generation:.2f} kWh')
